75f78428a679a4a2f5f5a8f245a879aa3a317175
abhishekvoid <ab14609a@gmail.com>
2026-04-12 19:41:13 +0530

    feat(routes): Driver Location creation endpoint

--- prac/prac_prject/core/urls.py
+++ prac/prac_prject/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import CreateOrderView
+from .views import CreateOrderView, DriverLocationUpdateView
 
 
 urlpatterns = [
     path("orders/", CreateOrderView.as_view(), name="orders"),
+    path("DriverLocation/", DriverLocationUpdateView.as_view(), name="Driver Location" )
 ]
